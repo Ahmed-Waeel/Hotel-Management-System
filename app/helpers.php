5ee0f55b91fca3bff28c02977d39f88ceee6e201
Ahmed Wael <ahmed.wael7822@gmail.com>
2024-05-01 08:22:09 +0300

    upload file helper function

--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,10 @@
 <?php
 
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Str;
+
 if (! function_exists('route_allowed')) {
     /**
      * Check if the gate allows the given permission.
@@ -12,5 +17,25 @@ if (! function_exists('route_allowed')) {
             return \Spatie\Permission\Models\Permission::whereName($route)->doesntExist() ||
                 \Illuminate\Support\Facades\Gate::allows($route);
         });
+    }
+}
+
+if (! function_exists('upload_file')) {
+    /**
+     * Upload file to path.
+     */
+    function upload_file(UploadedFile $file, string $path = ''): string
+    {
+        $path = 'uploads/' . $path;
+        $directory = dirname(public_path($path));
+        File::ensureDirectoryExists($directory, 0755, true);
+
+        $extension = $file->getClientOriginalExtension();
+        $originalName = basename($file->getClientOriginalName(), '.' . $extension);
+        $filename = Str::slug($originalName) . '-' . Str::random(8) . '.' . $extension;
+
+        $file->move($path, $filename);
+
+        return URL::to($path . '/' . $filename);
     }
 }
